2e804143813079a421f2d0294e5bf57512610fdb
xinyao-ai <love06130430@gmail.com>
2026-09-23 18:14:27 +0800

    Update xinyao_site_live_v2.js

--- xinyao_site_live_v2.js
+++ xinyao_site_live_v2.js
@@ -8,7 +8,7 @@
     'https://xinyao-atg-live.love06130430.workers.dev';
 
   const SCRIPT_URL =
-    'https://xinyao-ai.github.io/xinyao-ai/xinyao_ATG_live.user.js?v=315';
+    'https://xinyao-ai.github.io/xinyao-ai/xinyao_ATG_live.user.js?v=316';
 
   const GUIDE_IMAGES = {
     ios: './xinyao_guide_ios.png?v=203',
